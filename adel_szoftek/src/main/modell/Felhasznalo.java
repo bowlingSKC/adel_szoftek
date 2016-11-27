fb5ec5fc53cba27a2088a4909585eac5a00498c6
Adél <Adl@10.1.7.61>
2016-11-27 22:01:57 +0100

    Felhasználó properties

--- adel_szoftek/src/main/modell/Felhasznalo.java
+++ adel_szoftek/src/main/modell/Felhasznalo.java
@@ -2,4 +2,32 @@ package main.modell;
 
 public class Felhasznalo {
     
+    protected String szemszam;
+    protected String nev;
+    protected String jelszo;
+
+    public String getSzemszam() {
+        return szemszam;
+    }
+
+    public void setSzemszam(String szemszam) {
+        this.szemszam = szemszam;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public String getJelszo() {
+        return jelszo;
+    }
+
+    public void setJelszo(String jelszo) {
+        this.jelszo = jelszo;
+    }
+    
 }
